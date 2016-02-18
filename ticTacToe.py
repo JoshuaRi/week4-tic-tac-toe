09f57dd3cdb18b23967a213fa782043a45f4a472
JoshuaRi <Jritmo@mail.valenciacollege.edu>
2016-02-18 16:06:02 -0500

    Update ticTacToe.py

--- ticTacToe.py
+++ ticTacToe.py
@@ -9,7 +9,7 @@ def printBoard(board):
   print (board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
   print ('-+-+-)
   print (board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
-  print ('-+-+-)
+  print ('-+-+-')
   print (board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
   printBoard(theBoard)
 
